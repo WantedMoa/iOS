735d288d2ef962f7df30359be65caaec9eefac06
heoblitz <wone2287@gmail.com>
2021-11-16 22:28:46 +0900

    Add : CircleAnimation TestCode

--- Moa/Moa/Match/MatchViewController.swift
+++ Moa/Moa/Match/MatchViewController.swift
@@ -19,18 +19,20 @@ final class MatchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let contents: [MatchCircleContent] = [
-            (view: generateTestView(), radius: 64, startAngle: 90, duration: 10),
-            (view: generateTestView(), radius: 100, startAngle: 0, duration: 10),
-            (view: generateTestView(), radius: 120, startAngle: 180, duration: 10),
-            (view: generateTestView(), radius: 140, startAngle: 250, duration: 10)
+            (view: UIView(), radius: 128 / 2, startAngle: 90, duration: 0),
+            (view: generateImageView(32), radius: 234 / 2, startAngle: 100, duration: 10),
+            (view: generateImageView(42), radius: 336 / 2, startAngle: 100, duration: 10),
+            (view: generateImageView(54), radius: 336 / 2, startAngle: 30, duration: 10),
+            (view: generateImageView(54), radius: 336 / 2, startAngle: 270, duration: 10)
         ]
         
-        for content in contents {
+        for (index, content) in contents.enumerated() {
             animateCirlePath(
                 contentView: content.view,
                 circlePathRadius: content.radius,
                 circlePathStartAngle: content.startAngle,
-                animationDuration: content.duration
+                animationDuration: content.duration,
+                isHideLayer: index < 3 ? false : true
             )
         }
     }
@@ -45,11 +47,21 @@ extension MatchViewController {
         return squareView
     }
     
+    private func generateImageView(_ width: CGFloat) -> UIImageView {
+        let imageView = UIImageView()
+        imageView.frame = CGRect(x: 0, y: 0, width: width, height: width)
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = width / 2
+        imageView.image = UIImage(named: "TestProfile1")
+        return imageView
+    }
+    
     private func animateCirlePath(
         contentView target: UIView,
         circlePathRadius radius: Double,
         circlePathStartAngle startAngle: Double,
-        animationDuration duration: CFTimeInterval
+        animationDuration duration: CFTimeInterval,
+        isHideLayer: Bool = false
     ) {
         let circlePath = UIBezierPath(
             arcCenter: view.center,
@@ -64,16 +76,16 @@ extension MatchViewController {
         animation.repeatCount = MAXFLOAT
         animation.path = circlePath.cgPath
         animation.isRemovedOnCompletion = false
-
-        view.addSubview(target)
-        target.layer.add(animation, forKey: nil)
         
         let circleLayer = CAShapeLayer()
         circleLayer.path = circlePath.cgPath
-        circleLayer.lineWidth = 2
+        circleLayer.lineWidth = isHideLayer == false ? 2 : 0
         circleLayer.strokeColor = UIColor(rgb: 0xdddddd).cgColor
         circleLayer.fillColor = UIColor.clear.cgColor
         circleLayer.lineDashPattern = [4, 4]
+
         view.layer.addSublayer(circleLayer)
+        view.addSubview(target)
+        target.layer.add(animation, forKey: nil)
     }
 }
