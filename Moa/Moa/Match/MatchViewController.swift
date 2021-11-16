0fea2d3778ef1e653f5a078c356cc1533a4e5c08
heoblitz <wone2287@gmail.com>
2021-11-16 19:29:06 +0900

    Add : AnimateCirlePath

--- Moa/Moa/Match/MatchViewController.swift
+++ Moa/Moa/Match/MatchViewController.swift
@@ -6,24 +6,74 @@
 //
 
 import UIKit
+import SwiftUI
+
+typealias MatchCircleContent = (
+    view: UIView,
+    radius: CGFloat,
+    startAngle: Double,
+    duration: CFTimeInterval
+)
 
 final class MatchViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let contents: [MatchCircleContent] = [
+            (view: generateTestView(), radius: 64, startAngle: 90, duration: 10),
+            (view: generateTestView(), radius: 100, startAngle: 0, duration: 10),
+            (view: generateTestView(), radius: 120, startAngle: 180, duration: 10),
+            (view: generateTestView(), radius: 140, startAngle: 250, duration: 10)
+        ]
+        
+        for content in contents {
+            animateCirlePath(
+                contentView: content.view,
+                circlePathRadius: content.radius,
+                circlePathStartAngle: content.startAngle,
+                animationDuration: content.duration
+            )
+        }
     }
+}
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MAKR: - AnimateCirlePath
+extension MatchViewController {
+    private func generateTestView() -> UIView {
+        let squareView = UIView()
+        squareView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        squareView.backgroundColor = .lightGray
+        return squareView
     }
-    */
+    
+    private func animateCirlePath(
+        contentView target: UIView,
+        circlePathRadius radius: Double,
+        circlePathStartAngle startAngle: Double,
+        animationDuration duration: CFTimeInterval
+    ) {
+        let circlePath = UIBezierPath(
+            arcCenter: view.center,
+            radius: radius,
+            startAngle: Angle(degrees: startAngle).radians,
+            endAngle: Angle(degrees: startAngle + 360).radians,
+            clockwise: true
+        )
+        
+        let animation = CAKeyframeAnimation(keyPath: #keyPath(CALayer.position))
+        animation.duration = 10
+        animation.repeatCount = MAXFLOAT
+        animation.path = circlePath.cgPath
+        animation.isRemovedOnCompletion = false
 
+        view.addSubview(target)
+        target.layer.add(animation, forKey: nil)
+        
+        let circleLayer = CAShapeLayer()
+        circleLayer.path = circlePath.cgPath
+        circleLayer.lineWidth = 2
+        circleLayer.strokeColor = UIColor(rgb: 0xdddddd).cgColor
+        circleLayer.fillColor = UIColor.clear.cgColor
+        circleLayer.lineDashPattern = [4, 4]
+        view.layer.addSublayer(circleLayer)
+    }
 }
