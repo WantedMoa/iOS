b81bd80fce3993e5e6536b68c45b97ba277ad67c
heoblitz <wone2287@gmail.com>
2021-11-17 22:51:32 +0900

    Style : animate once

--- Moa/Moa/Match/MatchViewController.swift
+++ Moa/Moa/Match/MatchViewController.swift
@@ -16,24 +16,36 @@ typealias MatchCircleContent = (
 )
 
 final class MatchViewController: UIViewController {
+    
+    private var isFirstLoaded = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let contents: [MatchCircleContent] = [
-            (view: UIView(), radius: 128 / 2, startAngle: 90, duration: 0),
-            (view: generateImageView(32), radius: 234 / 2, startAngle: 100, duration: 10),
-            (view: generateImageView(42), radius: 336 / 2, startAngle: 100, duration: 10),
-            (view: generateImageView(54), radius: 336 / 2, startAngle: 30, duration: 10),
-            (view: generateImageView(54), radius: 336 / 2, startAngle: 270, duration: 10)
-        ]
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
-        for (index, content) in contents.enumerated() {
-            animateCirlePath(
-                contentView: content.view,
-                circlePathRadius: content.radius,
-                circlePathStartAngle: content.startAngle,
-                animationDuration: content.duration,
-                isHideLayer: index < 3 ? false : true
-            )
+        if isFirstLoaded {
+            let contents: [MatchCircleContent] = [
+                (view: UIView(), radius: 128 / 2, startAngle: 90, duration: 0),
+                (view: generateImageView(32), radius: 234 / 2, startAngle: 100, duration: 5),
+                (view: generateImageView(42), radius: 336 / 2, startAngle: 150, duration: 5),
+                (view: generateImageView(54), radius: 336 / 2, startAngle: 30, duration: 5),
+                (view: generateImageView(54), radius: 336 / 2, startAngle: 270, duration: 5)
+            ]
+            
+            for (index, content) in contents.enumerated() {
+                animateCirlePath(
+                    contentView: content.view,
+                    circlePathRadius: content.radius,
+                    circlePathStartAngle: content.startAngle,
+                    animationDuration: content.duration,
+                    isHideLayer: index < 3 ? false : true
+                )
+            }
+            
+            isFirstLoaded = false
         }
     }
 }
@@ -63,8 +75,9 @@ extension MatchViewController {
         animationDuration duration: CFTimeInterval,
         isHideLayer: Bool = false
     ) {
+        let center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
         let circlePath = UIBezierPath(
-            arcCenter: view.center,
+            arcCenter: center,
             radius: radius,
             startAngle: Angle(degrees: startAngle).radians,
             endAngle: Angle(degrees: startAngle + 360).radians,
@@ -72,8 +85,8 @@ extension MatchViewController {
         )
         
         let animation = CAKeyframeAnimation(keyPath: #keyPath(CALayer.position))
-        animation.duration = 10
-        animation.repeatCount = MAXFLOAT
+        animation.duration = duration
+        animation.repeatCount = 0 // MAXFLOAT
         animation.path = circlePath.cgPath
         animation.isRemovedOnCompletion = false
         
@@ -83,7 +96,14 @@ extension MatchViewController {
         circleLayer.strokeColor = UIColor(rgb: 0xdddddd).cgColor
         circleLayer.fillColor = UIColor.clear.cgColor
         circleLayer.lineDashPattern = [4, 4]
-
+        
+        target.frame = CGRect(
+            x: circlePath.currentPoint.x - (target.bounds.width / 2),
+            y: circlePath.currentPoint.y - (target.bounds.height / 2),
+            width: target.bounds.width,
+            height: target.bounds.height
+        )
+        
         view.layer.addSublayer(circleLayer)
         view.addSubview(target)
         target.layer.add(animation, forKey: nil)
